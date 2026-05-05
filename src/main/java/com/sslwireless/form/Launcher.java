cac25abc6563f3cddf5240e21505120210dce0a3
Nahid Mahmud <nahidmahmud826@gmail.com>
2026-05-05 21:25:02 +0600

    Delete src/main directory

--- src/main/java/com/sslwireless/form/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.sslwireless.form;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.sslwireless.form {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.sslwireless.form to javafx.fxml;
-    exports com.sslwireless.form;
-}
